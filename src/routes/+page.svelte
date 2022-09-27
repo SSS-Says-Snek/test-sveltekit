f6855db6a5a9f7b1df3ce16426a29e845051e05a
Brandon <bmcomis2018@gmail.com>
2022-09-26 21:53:43 -0500

    Test slugs and basic svelte stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let num_clicks: number = 0;
+</script>
+
+<div class="main">
+    <h1>Welcome to Snektodo!</h1>
+    <h3>You have clicked the button {num_clicks} time{num_clicks === 1 ? '' : 's'}!</h3>
+    <button on:click="{() => num_clicks++}">Click button</button>
+</div>
+
+<style>
+    .main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
